5d5b40acfcba3bd05e4b17bb71ac49216430f964
failure-to-resolve <transaction.failed.0@gmail.com>
2019-08-04 19:44:05 -0600

    Added a simple encryption function to the handler

---
+++ handler.py
@@ -0,0 +1,31 @@
+#! /usr/bin/python2
+
+a = open("testfile" ,"rb").read().strip()
+
+
+#Takes a single string, without newlines and uses a homebrewed encryption(Simple Xor), to prepare the string for network travel
+def processOutData(inString,sequenceNum):
+  interim = []
+  output = []
+  inputArray = list(inString)
+  inputArray.insert(0,str(len(inString)))
+  for eachChar in inputArray:
+    interim.append((ord(eachChar)))
+  for eachAscii in interim:
+    output.append(str(eachAscii^sequenceNum)+'X')
+    print eachAscii^sequenceNum
+  output.append(str(sequenceNum)+'S')
+  return ''.join(output[::-1])
+
+#reverses the above encryption
+def processInData(inString):
+  sequenceNum = inString.split('S')[0]
+  print sequenceNum
+  interimArray = []
+  formattedArray = inString.split('S').pop(1).split('X')
+  del formattedArray[-1]
+  for eachAscii in formattedArray:
+     interimArray.append(chr(int(eachAscii)^int(sequenceNum)))
+  return ''.join(interimArray[::-1])[1:]
+
+
